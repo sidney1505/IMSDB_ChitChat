9517a896b538a1053bdd24869c6cd123c34ca295
Sidney Bender - Betr. Monica <sbender@i14pc189.anthropomatik.kit.edu>
2018-06-19 15:28:35 +0200

    model 2018_06_19_15_28_35 started!

--- gan_chitchat_train.py
+++ gan_chitchat_train.py
@@ -2,8 +2,9 @@ from __future__ import print_function
 from __future__ import division
 
 import tensorflow as tf
+import numpy as np
 
-import time
+import time, git
 import argparse
 import os, code
 import json
@@ -11,12 +12,13 @@ import json
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--load_from", type=string, default=None, help="specify path, where config of loaded model lies")
+parser.add_argument("-l", "--load_from", default=None, help="specify path, where config of loaded model lies")
 parser.add_argument("-sp", "--skip_preprocessing", default=False, help="specify path, where config of loaded model lies")
 
 args = parser.parse_args()
 
-class Config(object):
+# config template
+class Config:
     """Holds model hyperparams and data information."""
 
     batch_size = 100
@@ -36,7 +38,7 @@ class Config(object):
     noisy_grads = False
 
     word2vec_init = False
-    embedding_init = np.sqrt(3)
+    embedding_init = 1.732 # sqrt(3)
 
     # NOTE not currently used hence non-sensical anneal_threshold
     anneal_threshold = 1000
@@ -49,16 +51,13 @@ class Config(object):
     max_allowed_inputs = 130
     num_train = 9000
 
-    babi_id = "1"
-    babi_test_id = ""
-
     train_mode = True
 
     dataset_location = '/home/sbender/Documents/IMSDB/movie_scripts_simple_dataset_babiformat.json'
     vocabulary_location = '/home/sbender/Documents/IMSDB/simple_dataset_vocabs.txt'
 
-    max_num_train_data = 1000000
-    max_num_test_data = 1000000
+    max_num_train_data = 1000
+    max_num_test_data = 1000
 
     preprocessed_dataset_location = '/cvhci/users/sbender/gan_chitchat/data/movie_scripts_simple_dataset_babiformat.npz'
     preprocess_data = True
@@ -67,20 +66,24 @@ class Config(object):
     is_loaded = False
     model_name = None
     base_model_dir = '/cvhci/users/sbender/gan_chitchat/models'
-    weights_path = base_model_dir + '/weights'
-    log_dir = base_model_dir + '/logs'
+    weights_path = None
+    log_dir = None
     num_train = None
     num_val = None
     num_test = None
+    git_hash = None
 
 def writeLog(path, value):
     writer = open(path,'w')
     writer.write(str(value))
     writer.close()
 
-if args.load_from != None:
+repo = git.Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
+# setup config
+if args.load_from == None:
     config = Config()
-    config.model_name = time.strftime("%Y-%m-%d %H %M").replace(' ','_').replace('-','_')
+    config.model_name = str(time.strftime("%Y-%m-%d %H %M %S").replace(' ','_').replace('-','_'))
     os.makedirs(config.base_model_dir + '/' + config.model_name)
     os.makedirs(config.base_model_dir + '/' + config.model_name + '/weights')
     config.log_dir = config.base_model_dir + '/' + config.model_name + '/logs'
@@ -90,7 +93,13 @@ if args.load_from != None:
     config.al_accuracy_log_path = config.log_dir + '/val_accuracy_log.txt'
     config.val_loss_log_path = config.log_dir + '/val_loss_log.txt'
     config.config_path = config.base_model_dir + '/' + config.model_name + '/config.json'
-    config_string = json.dump(config)
+    os.system('git add *.py')
+    os.system('git commit -m "model ' + config.model_name + ' started!"')
+    repo = git.Repo(search_parent_directories=True)
+    new_sha = repo.head.object.hexsha
+    config.git_hash = new_sha
+    code.interact(local=dict(globals(), **locals()))
+    config_string = json.dumps(config)
     config_writer = open(config.config_path,'w')
     config_writer.write(config_string)
     config_writer.close()
@@ -99,6 +108,9 @@ else:
     config_string = config_reader.read()
     config_reader.close()
     config = json.loads(config_string)
+    if config.git_hash != sha:
+        print('git hash doesnt match!')
+        code.interact(local=dict(globals(), **locals()))
 
 best_overall_val_loss = float('inf')
 
